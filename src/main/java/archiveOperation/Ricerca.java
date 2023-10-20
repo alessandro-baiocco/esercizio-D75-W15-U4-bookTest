d1a957c4f4199ba227919244f8a43c8411247ef6
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-10-20 15:11:35 +0200

    ricerca con vari parametri fatta

--- src/main/java/archiveOperation/Ricerca.java
+++ src/main/java/archiveOperation/Ricerca.java
@@ -1,26 +1,42 @@
 package archiveOperation;
 
-import java.util.InputMismatchException;
+import catalog.Material;
+import dao.MaterialDAO;
 
+import javax.persistence.TypedQuery;
+import java.util.InputMismatchException;
+import java.util.List;
+
+import static coso.Application.em;
 import static other.Tools.input;
 
+//query = "SELECT b FROM BlogPost b WHERE LOWER(b.title) LIKE LOWER(CONCAT(:title, '%'))"))
 public class Ricerca {
     public static void ricerca() {
+        MaterialDAO matDAO = new MaterialDAO(em);
         try {
             System.out.println("come vuoi ricercare ? isbn , anno o autore ");
             String researchFromUser = input.nextLine().toLowerCase().trim();
             switch (researchFromUser) {
                 case "isbn": {
                     System.out.println("inserire isbm da ricercare ");
-
+                    Material libroIsbn = matDAO.findById(input.nextInt());
+                    System.out.println(libroIsbn);
                     break;
                 }
                 case "anno": {
-
+                    System.out.println("inserire anno da ricercare ");
+                    int reseachYear = input.nextInt();
+                    List<Material> libriPerAnno = filterYear(reseachYear);
+                    libriPerAnno.forEach(System.out::println);
                     break;
                 }
-                case "autore": {
 
+                case "autore": {
+                    System.out.println("inserire autore da ricercare ");
+                    String reseachAuthor = input.nextLine();
+                    List<Material> libriPerAutore = filterAuthor(reseachAuthor);
+                    libriPerAutore.forEach(System.out::println);
                     break;
                 }
                 default: {
@@ -33,6 +49,19 @@ public class Ricerca {
             System.err.println(ex.getMessage());
         }
 
+    }
+
+
+    public static List<Material> filterYear(int anno) {
+        TypedQuery<Material> getAllQuery = em.createQuery("SELECT m FROM Material m WHERE m.annoDiPubblicazione = :anno", Material.class);
+        getAllQuery.setParameter("anno", anno);
+        return getAllQuery.getResultList();
+    }
+
+    public static List<Material> filterAuthor(String author) {
+        TypedQuery<Material> getAllQuery = em.createQuery("SELECT m FROM Material m WHERE LOWER(m.autore) LIKE LOWER(CONCAT(:author , '%'))", Material.class);
+        getAllQuery.setParameter("author", author);
+        return getAllQuery.getResultList();
     }
 
 }
